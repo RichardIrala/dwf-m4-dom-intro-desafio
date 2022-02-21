f48ab7cb8cf2a29b9298b51c0b76343f65b664dc
Richard Miguel Irala <richardirala47@hotmail.com>
2022-02-21 06:52:29 -0300

    Desafio completado

--- index.js
+++ index.js
@@ -25,6 +25,29 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function main() {
+  // Remove childrens of ulEl
+  const ulEl = document.querySelector(".lista");
+  const hijosLength = ulEl.children.length;
+
+  for (let x = 0; x < hijosLength; x++) {
+    ulEl.removeChild(ulEl.lastElementChild);
+  }
+  console.log(ulEl);
+  // New children elements of Ul
+  const cosasQueAprendimosLength = cosasQueAprendimos.length;
+  for (let x = 0; x < cosasQueAprendimosLength; x++) {
+    const elementos = ulEl.appendChild(document.createElement("li"));
+
+    elementos.textContent = cosasQueAprendimos[x].tema;
+    if (cosasQueAprendimos[x].class !== "") {
+      elementos.classList.add(cosasQueAprendimos[x].class);
+    }
+  }
+}
 
 main();
+
+// Eliminar todos los elementos <li> de la lista <ul class="lista"> (sin eliminar la lista <ul>)
+//Crear nuevos elementos <li> basados en el array arriba instanciado.
+//Publicar la página en GitHub Pages
